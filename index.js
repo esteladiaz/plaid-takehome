7bd69b47bbb0dc61cdd005edee0a352b9173307a
Estela Diaz <esteladiaz@fb.com>
2019-09-02 20:49:57 -0700

    add nodemon, express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 4040
+
+app.use(express.static('index.html'))
+
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at https://localhost:${PORT}/`)
+})
